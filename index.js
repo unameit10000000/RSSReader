61e274a994080e0b48cb3e2362f207f19d0db6c2
unameit10000000 <free4ms@gmail.com>
2025-01-05 14:57:29 +0100

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/rss', async function (req, res) {
+    const browserHeaders = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    };
+
+    const url = req.query.url;
+    const response = await axios.get(url, { headers: browserHeaders });
+
+    if(response.status !== 200) {
+        res.status(500).send('Error');
+        return;
+    }
+
+    res.send(response.data);
+});
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000!');
+});
